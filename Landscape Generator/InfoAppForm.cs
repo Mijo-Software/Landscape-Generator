7242f19e2bea4dba32dcf280cec3241c2e0faedf
Michael Johne <mjohne@users.noreply.github.com>
2019-12-07 03:33:27 +0100

    Add files via upload

---
+++ Landscape Generator/InfoAppForm.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace LandscapeGenerator
+{
+	/// <summary>
+	/// InfoAppForm
+	/// </summary>
+	public partial class InfoAppForm : Form
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public InfoAppForm() => InitializeComponent();
+
+		/// <summary>
+		/// Load the info form
+		/// </summary>
+		/// <param name="sender">object sender</param>
+		/// <param name="e">event arguments</param>
+		/// <remarks>The parameters <paramref name="e"/> and <paramref name="sender"/> are not needed, but must be indicated.</remarks>
+		private void InfoAppForm_Load(object sender, System.EventArgs e)
+		{
+		}
+	}
+}
---
+++ Landscape Generator/ViewTerrainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LandscapeGenerator
+{
+  partial class ViewTerrainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewTerrainForm));
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonOK
+			// 
+			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.buttonOK.Image = ((System.Drawing.Image)(resources.GetObject("buttonOK.Image")));
+			this.buttonOK.Location = new System.Drawing.Point(241, 539);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(75, 23);
+			this.buttonOK.TabIndex = 0;
+			this.buttonOK.Text = "&OK";
+			this.buttonOK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.buttonOK.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+			this.buttonOK.UseVisualStyleBackColor = true;
+			this.buttonOK.Click += new System.EventHandler(this.ButtonOK_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::LandscapeGenerator.Properties.Resources.terrain_test;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// ViewTerrainForm
+			// 
+			this.AcceptButton = this.buttonOK;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(534, 573);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.buttonOK);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "ViewTerrainForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Gesamtansicht des Terrain";
+			this.Load += new System.EventHandler(this.ViewTerrainForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button buttonOK;
+    private System.Windows.Forms.PictureBox pictureBox1;
+  }
+}
